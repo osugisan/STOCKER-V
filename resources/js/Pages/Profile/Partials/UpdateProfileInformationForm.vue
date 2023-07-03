6341909796907ddc1b76be160a40c955728f1e23
osugisan <es1000648548@gmail.com>
2023-07-04 00:05:31 +0900

    ログアウト処理追加

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,6 +1,6 @@
 <script setup>
 import InputError from '@/Components/InputError.vue';
-import { Link, useForm, usePage } from '@inertiajs/vue3';
+import { Link, useForm, usePage, router } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 const props = defineProps({
@@ -25,6 +25,10 @@ const blurFocus = () => {
 }
 
 const nameInput = ref(null)
+
+const logout = () => {
+    router.post(route('logout'))
+}
 </script>
 
 <template>
@@ -109,13 +113,13 @@ const nameInput = ref(null)
         </div>
     </div>
     <div class="flex justify-end">
-        <Link href="" class="text-red-500 font-medium inline-flex mt-4 pr-2">
+        <button @click="logout" class="text-red-500 font-medium inline-flex mt-4 pr-2">
             <svg class="inline-flex w-5 pb-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"></path>
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z"></path>
             </svg>
             サインアウト
-        </Link>
+        </button>
     </div>
 </div>
 
